fa6433c97e5228c7ca47d4d3541073f0c368d46a
Test User <luner-stone@mail.ru>
2022-06-13 08:21:37 +0300

    votes react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+import React from 'react';
 import './App.css';
+import {data} from "./Data/data"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(){
+  super()
+  this.state = {
+    data:data,
+    winner:0
+  }
+}
+handlerVote(index){
+const newData = [...this.state.data]
+newData[index].votes += 1
+let maxVotes=0
+let maxVotesIndex=0
+for(let [index, value] of newData.entries()){
+  if(value.votes > maxVotes){
+    maxVotes = value.votes
+    maxVotesIndex = index
+  }
+}
+  
+this.setState({data:newData, winner:maxVotesIndex})
+}  
+render(){
+  return(
+    <>
+    <div className="container">
+      <div></div>
+      <div className="container-data">
+      
+    {this.state.data.map((el, index)=>{
+      return(
+        <div className="container-description">
+          <img src={el.img} alt={el.name} className="img"/>
+          <h2>{el.name}</h2>
+          <button onClick={()=>this.handlerVote(index)}>Vote</button>
+          <h3>Vote Count: {el.votes} </h3>
+        </div>
+      )
+    })
+    }
+    
     </div>
-  );
+    <div></div>
+    <h2 className="winner">Winner {this.state.data[this.state.winner].name}</h2>
+    </div>
+    </>
+  )
+}
 }
 
 export default App;
---
+++ src/Data/data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        name:"React",
+        votes:0
+    },
+    {   
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+        name:"Angular",
+        votes:0
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png",
+        name:"Vue",
+        votes:0
+
+    }
+]
